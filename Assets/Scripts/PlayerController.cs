640f604ab95193ba9cf833d15f09b7864230cc1b
Kris <stawinogakris@gmail.com>
2024-10-28 14:16:28 +0100

    Add PlayerController and starting assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 4.0f;
+
+    [SerializeField]
+    private Rigidbody2D _rb;
+
+    void Awake()
+    {
+        if (_rb == null) // Unity objects should not use coalescing assignment.
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+        Assert.IsNotNull(_rb);
+    }
+
+    void Update()
+    {
+        float xDir = Input.GetAxis("Horizontal");
+        float yDir = Input.GetAxis("Vertical");
+        Vector2 dir = new(xDir, yDir);
+        if (dir != Vector2.zero)
+        {
+            Move(dir);
+        }
+    }
+
+    void Move(Vector2 dir)
+    {
+        _rb.velocity = dir * moveSpeed;
+    }
+}
